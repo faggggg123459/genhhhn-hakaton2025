d58cefbe4c08d390f5a4b3dcc70df8b230ec8ce8
OPELMOARRRRR <168326091+faggggg123459@users.noreply.github.com>
2025-05-02 07:25:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-// Анімація появи тексту
 document.addEventListener("DOMContentLoaded", () => {
-  const description = document.querySelector(".description");
-  description.style.opacity = "0";
   setTimeout(() => {
-    description.style.opacity = "1";
-    description.style.transition = "opacity 1.5s";
-  }, 500);
-});
-
-// Обробник кліку на кнопку START
-document.getElementById("startQuiz").addEventListener("click", () => {
-  alert("Готовий пройти тест на знання магічних героїв?");
-});
-
-// Динамічні ефекти на кнопці
-const button = document.querySelector(".glow-btn");
-button.addEventListener("mouseover", () => {
-  button.style.transform = "scale(1.1)";
-});
-button.addEventListener("mouseleave", () => {
-  button.style.transform = "scale(1)";
+    document.getElementById("loading-screen").style.display = "none";
+  }, 2500); // Завантаження триває 2,5 сек
 });
