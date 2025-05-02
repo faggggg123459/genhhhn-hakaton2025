335b990719457d09eb0e7f4104e23412150103e1
OPELMOARRRRR <168326091+faggggg123459@users.noreply.github.com>
2025-05-02 08:02:19 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    document.getElementById("loading-screen").style.display = "none";
-  }, 2500); // Завантаження триває 2,5 сек
-});
